6af83f591c389107effb2f0771534abcbf3f64d6
SaeedGo <40037292+SaeedGo@users.noreply.github.com>
2018-06-08 15:37:36 -0700

    FileAdded
    I added a file to TestProject

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DragDrop.Startup))]
+namespace DragDrop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
